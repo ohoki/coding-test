82a587ce10c21a9a109966eff5cee5d58dd1c2a9
Youngho <dudgh6237@naver.com>
2023-10-26 16:05:19 +0900

    tree - tree의 깊이 구하기 (dfs, bfs)

--- codingTest/src/practice/test/Tree.java
+++ codingTest/src/practice/test/Tree.java
@@ -1,6 +1,8 @@
 package practice.test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /*
  - Lowest Common Ancestor of a Binary Tree
@@ -13,9 +15,13 @@ import java.util.ArrayList;
  모든 Node.val은 Unique하다.
  p != q
  p,q는 모두 주어진 Tree에 속해 있다.
+ 
+ - tree의 깊이를 구하여라 -
 */
 
-public class Tree1 {
+
+
+public class Tree {
 	public static void main(String[] args) {
 		ArrayList<Node> nodes = new ArrayList<>(); 
 		//초기화
@@ -34,8 +40,11 @@ public class Tree1 {
 		
 		TreeNode tree = new TreeNode(nodes.get(3));
 		
+		
 		System.out.println(tree.lowestCommonNode(nodes.get(3), nodes.get(6), nodes.get(4)).val);
 		
+		System.out.println(tree.depth());
+		System.out.println(tree.depthDfs(tree.root));
 	}
 }
 class Node {
@@ -71,6 +80,76 @@ class TreeNode {
 		} else {
 			return left != null ? left : right; 
 		}
+	}
+	
+	//levelorder
+	public int depth() {
+		int maxDepth = 0;
+		if(root == null) {
+			return maxDepth;
+		}
+		
+		Queue<NodeInfo> q = new LinkedList<>();
+		NodeInfo nodeInfo = new NodeInfo();
+		
+		maxDepth = 1;
+		nodeInfo.setNode(root);
+		nodeInfo.setDepth(maxDepth);
+		q.add(nodeInfo);
+		
+		while(!q.isEmpty()) {
+			maxDepth = Math.max(maxDepth, q.peek().getDepth());
+			Node currNode = q.poll().getNode();
+			
+			if(currNode.left != null) {
+				NodeInfo nodeInfoLeft = new NodeInfo();
+				nodeInfoLeft.setDepth(maxDepth +1);
+				nodeInfoLeft.setNode(currNode.left);
+				q.add(nodeInfoLeft);
+			}
+			
+			if(currNode.right != null) {
+				NodeInfo nodeInfoRight = new NodeInfo();
+				nodeInfoRight.setDepth(maxDepth +1);
+				nodeInfoRight.setNode(currNode.right);
+				q.add(nodeInfoRight);
+			}
+		}
+		return maxDepth;
+	}
+	
+	//postorder
+	public int depthDfs(Node root) {
+		int maxDepth = 0;
+
+		if(root == null) {
+			return maxDepth;
+		}
+		
+		int left = depthDfs(root.left);
+		int right = depthDfs(root.right);
+		
+		maxDepth = Math.max(left, right);
+
+		return maxDepth + 1;
+	}
+}
+
+class NodeInfo {
+	Node node;
+	int depth;
+	
+	public Node getNode() {
+		return node;
+	}
+	public void setNode(Node node) {
+		this.node = node;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	public void setDepth(int depth) {
+		this.depth = depth;
 	}
 }
 
